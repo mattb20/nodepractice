0973824bac85de1fe21864b6c683ac93463fce45
Matt <matt_burgoyne@live.co.uk>
2019-01-08 17:42:10 +0000

    fix syntax issue with app id and code

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
-const request = require('request')
+const request = require('request');
 
-request({'https://weather.api.here.com/weather/1.0/report.xml?app_id={
-Kt4m1SROMh0MgQuxlUtJ}&app_code={wgHOQyUgIpbsHvc2WTG2Zg}&product=observation&name=Berlin'
-  url: '',
+request({
+  url: 'https://weather.api.here.com/weather/1.0/report.xml?app_id=Kt4m1SROMh0MgQuxlUtJ&app_code=wgHOQyUgIpbsHvc2WTG2Zg&product=observation&name=Berlin',
   json: true
 }, (error, response, body) => {
   console.log(body);
