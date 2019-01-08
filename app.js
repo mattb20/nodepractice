e7db3231cf0d618eef2bbf8195841440899de00a
Matt <matt_burgoyne@live.co.uk>
2019-01-08 17:53:41 +0000

    interpolate string using body of response from API

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const request = require('request');
 
 request({
-  url: 'https://weather.api.here.com/weather/1.0/report.xml?app_id=Kt4m1SROMh0MgQuxlUtJ&app_code=wgHOQyUgIpbsHvc2WTG2Zg&product=observation&name=Berlin',
+  url: 'https://weather.api.here.com/weather/1.0/report.json?app_id=Kt4m1SROMh0MgQuxlUtJ&app_code=wgHOQyUgIpbsHvc2WTG2Zg&product=observation&name=Berlin',
   json: true
 }, (error, response, body) => {
-  console.log(body);
+  console.log('The current weather in Berlin, Germany is: '+ body.observations.location[0].observation[0].description);
+  // let weatherResponse = response.observations.location.observation;
+  // console.log('The weather in Berlin, Germany is: ') + weatherResponse.temperatureDesc + weatherResponse.skyDescription;
 });
