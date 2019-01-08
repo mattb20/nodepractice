584b282c6c73cab24bb61f98b5dd7f55988394df
Matt <matt_burgoyne@live.co.uk>
2019-01-08 17:40:09 +0000

    log body of reponse from API

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const request = require('request')
 
-request({'https://weather.api.here.com/weather/1.0/report.xml?app_id={YOUR_APP_ID}&app_code={YOUR_APP_CODE}&product=observation&name=Berlin'
-  url: ''
+request({'https://weather.api.here.com/weather/1.0/report.xml?app_id={
+Kt4m1SROMh0MgQuxlUtJ}&app_code={wgHOQyUgIpbsHvc2WTG2Zg}&product=observation&name=Berlin'
+  url: '',
+  json: true
 }, (error, response, body) => {
-
+  console.log(body);
 });
